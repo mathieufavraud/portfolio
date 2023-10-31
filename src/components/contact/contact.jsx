ec8d257f006832292c477a86601dececcaf017a1
Mathieu Favraud <mathieu.favraud@gmail.com>
2023-10-31 13:41:18 +0100

    update

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,15 +1,34 @@
 import "./contact.scss"
+import emailjs from "@emailjs/browser"
+import { useRef } from "react";
 import {BsFillSendFill} from "react-icons/bs"
 
 const contact = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault(); 
+        console.log("email");
+     
+        emailjs.sendForm("service_324vnbr", "template_e8rku3y", form.current, "I5XgpIfgH1u71e0GV")
+          .then((result) => {
+              console.log("email success", result)
+          }, (error) => {
+              console.error("email failed", error)
+          });
+      };
+     
+
+
+
     return (
         <section id="contact" className="section_title_left">
             <h2>Contactez moi :</h2>
-            <form action="">
-                <input className="text_form" type="text" id="name" placeholder="Votre nom"/>
-                <input className="text_form" type="email" id="mail" placeholder="Votre email" required/>
-                <textarea className="text_form" id="message" placeholder="Que voulez vous dire ?"></textarea>
-                <button>Envoyer <BsFillSendFill/></button>
+            <form ref={form} onSubmit={sendEmail}>
+                <input className="text_form" type="text" id="name" name="user_name" placeholder="Votre nom"/>
+                <input className="text_form" type="email" id="mail" name="user_email" placeholder="Votre email" required/>
+                <textarea className="text_form" id="message" name="message" placeholder="Que voulez vous dire ?"></textarea>
+                <button><input type="submit" value="Envoyer"/> Envoyer <BsFillSendFill/></button>
             </form>
         </section>
     );
@@ -17,4 +36,4 @@ const contact = () => {
 
 export default contact;
 
-//configurer un service email
+//service mail ne fonctionne que partiellement
